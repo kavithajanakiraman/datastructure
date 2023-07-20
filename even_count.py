eb012d458f7c3326606c7a08d4185ad61aba8d6b
kavithajanakiraman <j.kavi539@gmail.com>
2023-07-20 16:54:07 +0530

    Add files via upload

---
+++ even_count.py
@@ -0,0 +1,22 @@
+# Find even numbers count in list and print it
+# print("----Even number count---------")
+# list1 = [3, 5, 20, 6, 8, 2, 7, 3, 1, 6, 2, 6, 8, 4, 2]
+# counter = 0
+# for i in list1:
+#     if i % 2 == 0:
+#         counter = counter + 1
+#         print("even number in list", i)
+# print(f"Total even count is {counter}")
+
+
+# print("----Even number count tuple---------")
+tuple1 = (3, 5, 20, 6, 8, 2, 7, 3, 1, 6, 2, 6, 8, 4, 2)
+
+sum1 = 0
+for i in tuple1:
+    if i % 2 == 0:
+        sum1 = sum1 + i
+print(sum1)
+
+
+
